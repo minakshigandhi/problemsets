9c8ae00bd9d1495856cde4db1fb37330fe15cc3c
pfb04 <pfb04@info04.cshl.edu>
2023-10-17 11:45:35 -0400

    firstscript.py

---
+++ firstscript.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+#My_name='Sofia'
+#print(My_name)
+#My_favorite_color='Green'
+#print(My_favorite_color)
+
+#My_favorite_activity='trying to make it coding'
+#My_favorite_animal='penguins'
+#print(My_favorite_activity, My_favorite_animal)
+
+#print(f"My name is {My_name}", f"My favorite color is {My_favorite_color}", f"My favorite animal is {My_favorite_animal}")
+
+import sys
+input_name = sys.argv[1]
+input_color = sys.argv[2]
+input_activity = sys.argv[3]
+input_animal = sys.argv[4]
+
+print(input_name,input_color,input_activity, input_animal)
+
