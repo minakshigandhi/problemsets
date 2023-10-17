6d35892d6d2235a29f8c2ee06135afbe15b9fc29
pfb04 <pfb04@info04.cshl.edu>
2023-10-17 13:27:01 -0400

    scripttrial.py

---
+++ scripttrial.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+input_name = sys.argv[1]
+input_color = sys.argv[2]
+input_activity = sys.argv[3]
+input_animal = sys.argv[4]
+
+print(input_name, 'loves color', input_color, 'and loves', input_activity, 'with', input_animal)
